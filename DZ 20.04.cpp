c172ac1f34a1eaf8396b41da941dbafd246f6635
Pavel Stepanov <102959930+Koyotis@users.noreply.github.com>
2022-04-20 14:24:55 +0500

    Домашнее задание на 20.04
    Создание, наполнение, вывод, удаление двумерного массива

---
+++ DZ 20.04.cpp
@@ -0,0 +1,67 @@
+﻿
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using std::cout;
+
+void fill (int num[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        num[i] = rand() % 100; 
+    }
+}
+void print(int num[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << num[i] << " ";
+    }
+}
+void  fill_2array(int** arr, int rows, int cols, int max)
+{
+    srand(time(0));
+    for (int i = 0; i < rows; i++)
+        arr[i] = new int[cols];
+    
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            arr[i][j] = rand() % max;
+        }
+    }
+
+}
+
+void print_2array(int** arr, int rows, int cols)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << std::endl;
+    }
+}
+
+void delete_2arr(int** arr, int rows)
+{
+    for (int i = 0; i < rows; i++)
+        delete arr[i];
+    delete arr;
+}
+int main()
+{
+    srand(time(0));
+    int rows, cols;
+    std:: cin >> rows >> cols;
+    int max = 25;
+    int** arr = new int* [rows];
+
+    fill_2array(arr, rows, cols, max);
+    
+    print_2array(arr, rows, cols);
+    delete_2arr(arr, rows);
+}
